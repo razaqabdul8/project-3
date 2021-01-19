7ed493c4016ec35e55c5c7aae6de9df6db5fee8e
Yigezu Mulugeta <Yigezumb@gmail.com>
2021-01-18 19:01:02 -0500

    updated user login and register code

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,38 +4,33 @@ const bcrypt = require("bcrypt");
   const router = express.Router();
   // signup route
   // Matches with "/api/user/signup"
-  router.post("/signup", async (req, res) => {
-    const body = req.body;
-
-    if (!(body.email && body.password)) {
-      return res.status(400).send({ error: "Data not formatted properly" });
+  router.post("/register", async (request, response) => {
+    try {
+        var user = new User(request.body);
+        var result = await user.save();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
     }
-
-    // createing a new mongoose doc from user data
-    const user = new User(body);
-    // generate salt to hash password
-    const salt = await bcrypt.genSalt(10);
-    // now we set user password to hashed password
-    user.password = await bcrypt.hash(user.password, salt);
-    user.save().then((doc) => res.status(201).send(doc));
-  });
+});
 
   // login route
    // Matches with "/api/user/login"
-  router.post("/login", async (req, res) => {
-    const body = req.body;
-    const user = await User.findOne({ email: body.email });
-    if (user) {
-      // check user password with hashed password stored in the database
-      const validPassword = await bcrypt.compare(body.password, user.password);
-      if (validPassword) {
-        res.status(200).json({ message: "Valid password" });
-      } else {
-        res.status(400).json({ error: "Invalid Password" });
-      }
-    } else {
-      res.status(401).json({ error: "User does not exist" });
+  router.post("/login", async (request, response) => {
+    try {
+        var user = await User.findOne({ email: request.body.email }).exec();
+        if(!user) {
+            return response.status(400).send({ message: "The username does not exist" });
+        }
+        user.comparePassword(request.body.password, (error, match) => {
+            if(!match) {
+                return response.status(400).send({ message: "The password is invalid" });
+            }
+        });
+        response.send({ message: "The username and password combination is correct!" });
+    } catch (error) {
+        response.status(500).send(error);
     }
-  });
+});
 
   module.exports = router;
